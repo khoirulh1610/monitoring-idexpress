2fe9fb13ef95fa4b09497483cd4123f80a399578
Khoirul Hdamini <khoirulh1610@gmail.com>
2022-12-07 15:18:12 +0700

    init

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,10 +10,10 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         $delivered = Paket::where('operationType', '10')->count();
-        $gagal = Paket::where('operationType', ['18','19'])->count();
-        $onprocess = Paket::where('operationType',['00','04','05','09'])->count();
-        $plus3 = Paket::where('operationType',['00','04','05','09'])->where('overdue','>',3)->count();
-        $plus7 = Paket::where('operationType',['00','04','05','09'])->where('overdue','>',7)->count();
+        $gagal = Paket::whereIn('operationType', ['18','19'])->count();
+        $onprocess = Paket::whereIn('operationType',['00','04','05','09'])->count();
+        $plus3 = Paket::whereIn('operationType',['00','04','05','09'])->where('overdue','>',3)->count();
+        $plus7 = Paket::whereIn('operationType',['00','04','05','09'])->where('overdue','>',7)->count();
         $paket = Paket::take(10)->orderBy('id','desc')->get();
         return view('dashboard', compact('delivered', 'onprocess', 'gagal','paket','plus3','plus7'));
     }
