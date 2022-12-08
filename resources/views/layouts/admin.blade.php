0249313c46759251e7debdc5c840890d758b4dca
Khoirul Hdamini <khoirulh1610@gmail.com>
2022-12-08 14:32:25 +0700

    update notif

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -91,11 +91,11 @@
 							<ul class="notification-list">
 							    @foreach($paketgagal as $pk)
 								<li class="notification-message">
-									<a href="activities.html">
+									<a href="{{url('paket/show/'.$pk->id)}}">
 										<div class="media d-flex">
-											<span class="avatar avatar-sm">
-												<img class="avatar-img rounded-circle" alt="" src="/assets/img/profiles/avatar-02.jpg">
-											</span>
+											<!--<span class="avatar avatar-sm">-->
+											<!--	<img class="avatar-img rounded-circle" alt="" src="/assets/img/profiles/avatar-02.jpg">-->
+											<!--</span>-->
 											<div class="media-body">
 												<p class="noti-details"><span class="noti-title">{{$pk->waybill_no}}</span>													{{$pk->waybill_status}}
 												<p class="noti-time"><span class="notification-time">{{$pk->pick_up_end_time}}</span>
@@ -106,66 +106,7 @@
 								</li>
 								@endforeach
 								
-								<!--<li class="notification-message">-->
-								<!--	<a href="activities.html">-->
-								<!--		<div class="media d-flex">-->
-								<!--			<span class="avatar avatar-sm">-->
-								<!--				<img class="avatar-img rounded-circle" alt="" src="/assets/img/profiles/avatar-03.jpg">-->
-								<!--			</span>-->
-								<!--			<div class="media-body">-->
-								<!--				<p class="noti-details"><span class="noti-title">Marie Canales</span>-->
-								<!--					has accepted your estimate <span class="noti-title">#GTR458789</span></p>-->
-								<!--				<p class="noti-time"><span class="notification-time">6 mins ago</span>-->
-								<!--				</p>-->
-								<!--			</div>-->
-								<!--		</div>-->
-								<!--	</a>-->
-								<!--</li>-->
-								<!--<li class="notification-message">-->
-								<!--	<a href="activities.html">-->
-								<!--		<div class="media d-flex">-->
-								<!--			<div class="avatar avatar-sm">-->
-								<!--				<span class="avatar-title rounded-circle bg-primary-light"><i class="far fa-user"></i></span>-->
-								<!--			</div>-->
-								<!--			<div class="media-body">-->
-								<!--				<p class="noti-details"><span class="noti-title">New user-->
-								<!--						registered</span></p>-->
-								<!--				<p class="noti-time"><span class="notification-time">8 mins ago</span>-->
-								<!--				</p>-->
-								<!--			</div>-->
-								<!--		</div>-->
-								<!--	</a>-->
-								<!--</li>-->
-								<!--<li class="notification-message">-->
-								<!--	<a href="activities.html">-->
-								<!--		<div class="media d-flex">-->
-								<!--			<span class="avatar avatar-sm">-->
-								<!--				<img class="avatar-img rounded-circle" alt="" src="/assets/img/profiles/avatar-04.jpg">-->
-								<!--			</span>-->
-								<!--			<div class="media-body">-->
-								<!--				<p class="noti-details"><span class="noti-title">Barbara Moore</span>-->
-								<!--					declined the invoice <span class="noti-title">#RDW026896</span></p>-->
-								<!--				<p class="noti-time"><span class="notification-time">12 mins-->
-								<!--						ago</span></p>-->
-								<!--			</div>-->
-								<!--		</div>-->
-								<!--	</a>-->
-								<!--</li>-->
-								<!--<li class="notification-message">-->
-								<!--	<a href="activities.html">-->
-								<!--		<div class="media d-flex">-->
-								<!--			<div class="avatar avatar-sm">-->
-								<!--				<span class="avatar-title rounded-circle bg-info-light"><i class="far fa-comment"></i></span>-->
-								<!--			</div>-->
-								<!--			<div class="media-body">-->
-								<!--				<p class="noti-details"><span class="noti-title">You have received a-->
-								<!--						new message</span></p>-->
-								<!--				<p class="noti-time"><span class="notification-time">2 days ago</span>-->
-								<!--				</p>-->
-								<!--			</div>-->
-								<!--		</div>-->
-								<!--	</a>-->
-								<!--</li>-->
+								
 							</ul>
 						</div>
 						<div class="topnav-dropdown-footer">
